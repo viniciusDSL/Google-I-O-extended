2c6f08d0100929bef906c46d87c4b9a1e890c90c
Vinicius Da Silva Limachi <viniciusDSL@users.noreply.github.com>
2015-06-16 01:51:30 -0400

    Update WelcomeActivity.java

--- GDG/GoogleIOextended/app/src/main/java/com/vinidsl/googleioextended/ui/WelcomeActivity.java
+++ GDG/GoogleIOextended/app/src/main/java/com/vinidsl/googleioextended/ui/WelcomeActivity.java
@@ -55,11 +55,6 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
